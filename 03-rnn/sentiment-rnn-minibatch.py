d7c8699814328b3146db0aace2043345169e35dc
mjeensung <mjtwins89@gmail.com>
2024-09-16 17:06:21 +0900

    03-rnn

--- 03-rnn/sentiment-rnn-minibatch.py
+++ 03-rnn/sentiment-rnn-minibatch.py
@@ -6,6 +6,8 @@ import time
 import random
 import numpy as np
 from tqdm import tqdm
+from torch.utils.data import DataLoader, Dataset
+from torch.nn.utils.rnn import pad_sequence
 
 # Functions to read in the corpus
 w2i = defaultdict(lambda: len(w2i))
@@ -20,10 +22,34 @@ def read_dataset(filename):
 
 # Read in the data
 train_data = list(read_dataset("../data/classes/train.txt"))
-w2i = defaultdict(lambda: UNK, w2i)
+w2i = defaultdict(lambda: UNK, w2i)  # Now UNK is used for unknown words
 dev_data = list(read_dataset("../data/classes/test.txt"))
 nwords = len(w2i)
 ntags = len(t2i)
+
+# Custom dataset class to work with DataLoader
+class TextDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+# Collate function for padding sentences in each batch
+def collate_fn(batch):
+    sentences, tags = zip(*batch)
+    sentences = [torch.tensor(sent) for sent in sentences]
+    tags = torch.tensor(tags)
+    padded_sentences = pad_sequence(sentences, batch_first=True, padding_value=UNK)
+    return padded_sentences, tags
+
+# Define DataLoader
+BATCH_SIZE = 32
+train_loader = DataLoader(TextDataset(train_data), batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
+dev_loader = DataLoader(TextDataset(dev_data), batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_fn)
 
 # Define the model
 EMB_SIZE = 64
@@ -47,10 +73,10 @@ class RNNModel(nn.Module):
             elif 'bias' in name:
                 param.data.fill_(0)  # Initialize biases to zero
     
-    def forward(self, sentence):
-        embeds = self.embedding(sentence) # [len(sentence) x emb_size]
-        rnn_out, _ = self.rnn(embeds)# [len(sentence) x hidden_size]
-        logits = self.fc(rnn_out[-1])
+    def forward(self, sentences):
+        embeds = self.embedding(sentences)  # [batch_size x len(sentences[1]) x emb_size]
+        rnn_out, _ = self.rnn(embeds)       # [batch_size x len(sentences[1]) x hidden_size]
+        logits = self.fc(rnn_out[:, -1, :]) # Use the last hidden state for classification
         return logits
 
 model = RNNModel(nwords, ntags, EMB_SIZE, HID_SIZE)
@@ -60,33 +86,28 @@ criterion = nn.CrossEntropyLoss()
 # Training loop
 for ITER in range(100):
     # Perform training
-    random.shuffle(train_data)
     train_loss = 0.0
     start = time.time()
     model.train()
 
-    for words, tag in tqdm(train_data):
-        words = torch.tensor(words)
-        tag = torch.tensor(tag)
-
+    for sentences, tags in tqdm(train_loader):
         logits = model(words)
-        loss = criterion(logits, tag)
+        loss = criterion(logits, tags)
         train_loss += loss.item()
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
     
-    print(f"iter {ITER}: train loss/sent={train_loss / len(train):.4f}, time={time.time() - start:.2f}s")
+    print(f"iter {ITER}: train loss/sent={train_loss / len(train_loader):.4f}, time={time.time() - start:.2f}s")
 
     # Perform evaluation
     model.eval()
     test_correct = 0.0
     with torch.no_grad():
-        for words, tag in tqdm(dev_data):
-            words = torch.tensor(words)
-            logits = model(words)
-            predict = torch.argmax(logits).item()
-            if predict == tag:
-                test_correct += 1
-    print(f"iter {ITER}: test acc={test_correct / len(dev):.4f}")
+        for sentences, tags in tqdm(dev_loader):
+            logits = model(sentences)
+            predict = torch.argmax(logits, dim=1)
+            test_correct += (predict == tags).sum().item()
+    
+    print(f"iter {ITER}: test acc={test_correct / len(dev_data):.4f}")
